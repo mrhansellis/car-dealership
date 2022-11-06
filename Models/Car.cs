313901ccc91a8a27b87a9a3e9662e7753dc6aebb
Hans Ellis <bullhorn@gmail.com>
2022-11-06 14:16:14 -0800

    add dakar rally analysis feature Co-authored-by: Jonathan Liera <jonliera1@gmail.com> Co-authored-by: David Marchuk <davormarchuk@gmail.com> Co-authored by: Hans Ellis <bullhorn@gmail.com> Co-authored by: Brett Roper <brettroper22@gmail.com> Co-authored-by: Peter Philavanh <efl2ice89@gmail.com>

--- Models/Car.cs
+++ Models/Car.cs
@@ -7,12 +7,16 @@ namespace Dealership.Models {
     public string MakeModel { get; set; }
     public int Price { get; set; }
     public int Miles { get; set; }
+    public int Weight { get; set; } // Weight in lbs.
+    public bool FuelType { get; set; } // True would mean fuel is gasoline, False would mean fuel is diesel.
 
-    public Car(string makeModel, int price, int miles)
+    public Car(string makeModel, int price, int miles, int weight, bool fuelType)
     {
       MakeModel = makeModel;
       Price = price;
       Miles = miles;
+      Weight = weight;
+      FuelType = fuelType;
     }
 
     public string GetMakeModel()
@@ -24,12 +28,19 @@ namespace Dealership.Models {
     {
       return Price;
     }
-
     public int GetMiles()
     {
       return Miles;
     }
-
+    public int GetWeight()
+    {
+      return Weight;
+    }
+    public bool GetFuelType()
+    {
+      return FuelType;
+    }
+    
     public void SetPrice(int newPrice)
     {
       Price = newPrice;
@@ -48,6 +59,46 @@ namespace Dealership.Models {
     public static string MakeSound(string sound)
     {
       return "our cars sound like " + sound;
+    }
+
+    public void SalesPrice()
+    {
+      Price = Convert.ToInt32(GetPrice() / 1.25);
+    }
+
+      public int Rally()
+    {
+      int quality = 10;
+
+      if (Miles > 100000)
+      {
+        quality -= 4;
+      }
+      else if (Miles > 50000)
+      {
+         quality -= 2;
+      }
+      else if (Miles > 10000)
+      {
+         quality -= 1;
+      }
+
+      if (Weight > 4000)
+      {
+        quality -= 4;
+      }
+      else if (Weight > 3000)
+      {
+         quality -= 2;
+      }
+      else if (Weight > 2000)
+      {
+         quality -= 1;
+      }
+
+      if(FuelType){quality -= 1;}
+
+      return quality;
     }
   }
 }
--- Program.cs
+++ Program.cs
@@ -8,12 +8,15 @@ namespace Dealership
   {
     public static void Main()
     {
-      Car volkswagen = new Car("1974 Volkswagen Thing", 1100, 368792);
-      Car yugo = new Car("1980 Yugo Koral", 700, 56000);
-      Car ford = new Car("1988 Ford Country Squire", 1400, 239001);
-      Car amc = new Car("1976 AMC Pacer", 400, 198000);
+      Car volkswagen = new Car("1974 Volkswagen Thing", 1100, 368792, 1955, true);
+      Car yugo = new Car("1980 Yugo Koral", 700, 56000, 1819, true);
+      Car ford = new Car("1988 Ford Country Squire", 1400, 239001, 3935, true);
+      Car amc = new Car("1976 AMC Pacer", 400, 198000, 3000, true);
+      Car rabbit = new Car("1977 Volkwagen Rabbit", 2300, 420420, 1946, false);
+      Car lada = new Car("1984 Lada VAZ-2103", 800, 230400, 1962, false);
+      Car hudson = new Car("1933 Hudson Terraplane", 10000, 350781, 2670, true);
 
-      List<Car> Cars = new List<Car>() { volkswagen, yugo, ford, amc };
+      List<Car> Cars = new List<Car>() { volkswagen, yugo, ford, amc, rabbit, lada, hudson };
 
       Console.WriteLine("Would you like to hear about our sales event? [Y or N]");
       string answer = Console.ReadLine();
@@ -41,12 +44,13 @@ namespace Dealership
         Console.WriteLine("----------------------");
         Console.WriteLine(automobile.GetMakeModel());
         Console.WriteLine(automobile.GetMiles() + " miles");
-        if (automobile.MakeModel == "1974 Volkswagen Thing" || automobile.MakeModel == "1980 Yugo Koral") 
+        Console.WriteLine("In the Dakar rally the " + automobile.GetMakeModel() + " has a quality score of: " + automobile.Rally() + " out of 10");
+        if (automobile.MakeModel == "1974 Volkswagen Thing" || automobile.MakeModel == "1980 Yugo Koral" || automobile.MakeModel == "1984 Lada VAZ-2103") 
         {
           automobile.SalesPrice();
         }
         Console.WriteLine("$" + automobile.GetPrice());
-      }
+      }    
     }
   }
 }
