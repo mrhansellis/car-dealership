275efeb2d091d5f4fba46e291ddc62afe9e6b199
Hans Ellis <bullhorn@gmail.com>
2022-11-06 12:16:28 -0800

    add sales event feature Co-authored-by: Jonathan Liera <jonliera1@gmail.com> Co-authored-by: David Marchuk <davormarchuk@gmail.com> Co-authored by: Hans Ellis <bullhorn@gmail.com>

--- Program.cs
+++ Program.cs
@@ -10,17 +10,18 @@ namespace Dealership
     {
       Car volkswagen = new Car("1974 Volkswagen Thing", 1100, 368792);
       Car yugo = new Car("1980 Yugo Koral", 700, 56000);
-      Console.WriteLine(yugo.MakeModel);
-      yugo.MakeModel = "Worst Car Ever";
-      Console.WriteLine(yugo.MakeModel);
       Car ford = new Car("1988 Ford Country Squire", 1400, 239001);
       Car amc = new Car("1976 AMC Pacer", 400, 198000);
 
       List<Car> Cars = new List<Car>() { volkswagen, yugo, ford, amc };
 
-      yugo.SetPrice(300);
-      ford.SetModel("Hans Hans");
-
+      Console.WriteLine("Would you like to hear about our sales event? [Y or N]");
+      string answer = Console.ReadLine();
+      if (answer == "Y" || answer == "y")
+      {
+        //Console.WriteLine("Your new price is: " + yugo.GetPrice());
+      }
+      
       Console.WriteLine("Enter a maximum price: ");
       string stringMaxPrice = Console.ReadLine();
       int maxPrice = int.Parse(stringMaxPrice);
@@ -40,8 +41,11 @@ namespace Dealership
         Console.WriteLine("----------------------");
         Console.WriteLine(automobile.GetMakeModel());
         Console.WriteLine(automobile.GetMiles() + " miles");
+        if (automobile.MakeModel == "1974 Volkswagen Thing" || automobile.MakeModel == "1980 Yugo Koral") 
+        {
+          automobile.SalesPrice();
+        }
         Console.WriteLine("$" + automobile.GetPrice());
-        Console.WriteLine(Car.MakeSound("Rawr xD"));
       }
     }
   }
